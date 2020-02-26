3ff8e86ae4f74e2356a2b98015aa4bdc21dcb4a5
peyo <peteryyoon@gmail.com>
2020-02-26 14:41:10 -0800

    adding all current files.

---
+++ src/BookmarkItem/BookmarkItem.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Rating from "../Rating/Rating";
+import BookmarksContext from "../BookmarksContext";
+import config from "../config";
+import "./BookmarkItem.css";
+
+function deleteBookmarkRequest(bookmarkId, callback) {
+  fetch(config.API_ENDPOINT + `/${bookmarkId}`, {
+    headers: {
+      "authorization": `bearer ${config.API_KEY}`
+    }
+  })
+    .then(res => {
+      if (!res.ok) {
+        return res.json().then(error => {
+        throw error
+      })
+      }
+      return res.json()
+    })
+    .then(data => {
+    callback(bookmarkId)
+    })
+    .catch(error => {
+    console.error(error)
+  })
+}
+
+export default function BookmarkItem(props) {
+  return (
+    <BookmarksContext.Consumer>
+      {(context) => (
+        <li className="BookmarkItem">
+          <div className="BookmarkItem__row">
+            <h3 className="BookmarkItem__title">
+              <a href={props.url} target="_blank" rel="noopener noreferrer">
+                {props.title}
+              </a>
+            </h3>
+            <Rating value={props.rating} />
+          </div>
+          <p className="BookmarkItem__description">{props.description}</p>
+          <div className="BookmarkItem__buttons">
+            <button
+                className="BookmarkItem__description"
+                onClick={() => {
+                  deleteBookmarkRequest(
+                    props.id,
+                    context.deleteBookmark,
+                  )
+                }}
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      )}
+    </BookmarksContext.Consumer>
+  );
+}
+
+BookmarkItem.defaultProps = {
+  onClickDelete: () => {}
+};
