5ee04715c8a43319d56ac3dc4fc51d3101447f31
peyo <peteryyoon@gmail.com>
2020-04-13 17:27:26 -0700

    formatted

--- src/EditBookmark/EditBookmark.js
+++ src/EditBookmark/EditBookmark.js
@@ -1,133 +1,116 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import BookmarksContext from '../BookmarksContext';
-import config from '../config'
-import './EditBookmark.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import BookmarksContext from "../BookmarksContext";
+import config from "../config";
+import "./EditBookmark.css";
 
-const Required = () => (
-  <span className='EditBookmark__required'>*</span>
-)
+const Required = () => <span className="EditBookmark__required">*</span>;
 
 class EditBookmark extends Component {
   static propTypes = {
     match: PropTypes.shape({
       params: PropTypes.object,
-    })
+    }),
   };
 
   static contextType = BookmarksContext;
 
   state = {
     error: null,
-    id: '',
-    title: '',
-    url: '',
-    description: '',
+    id: "",
+    title: "",
+    url: "",
+    description: "",
     rating: 1,
   };
 
   componentDidMount() {
-    const { bookmarkId } = this.props.match.params
+    const { bookmarkId } = this.props.match.params;
     fetch(config.API_ENDPOINT + `/${bookmarkId}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'authorization': `Bearer ${config.API_TOKEN}`
-      }
+        authorization: `Bearer ${config.API_TOKEN}`,
+      },
     })
-      .then(res => {
-        if (!res.ok)
-          return res.json().then(error => Promise.reject(error))
+      .then((res) => {
+        if (!res.ok) return res.json().then((error) => Promise.reject(error));
 
-        return res.json()
+        return res.json();
       })
-      .then(res => {
+      .then((res) => {
         this.setState({
           id: res.id,
           title: res.title,
           url: res.url,
           description: res.description,
           rating: res.rating,
-        })
+        });
       })
-      .catch(error => {
-        console.error(error)
-        this.setState({ error })
-      })
+      .catch((error) => {
+        console.error(error);
+        this.setState({ error });
+      });
   }
 
-  handleChange = event => {
+  handleChange = (event) => {
     let name = event.target.getAttribute("name");
     this.setState({
-      [name]: event.target.value
+      [name]: event.target.value,
     });
-  }
+  };
 
-  handleSubmit = e => {
-    e.preventDefault()
-    const { bookmarkId } = this.props.match.params
-    const { id, title, url, description, rating } = this.state
-    const newBookmark = { id, title, url, description, rating }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { bookmarkId } = this.props.match.params;
+    const { id, title, url, description, rating } = this.state;
+    const newBookmark = { id, title, url, description, rating };
     fetch(config.API_ENDPOINT + `/${bookmarkId}`, {
-      method: 'PATCH',
+      method: "PATCH",
       body: JSON.stringify(newBookmark),
       headers: {
-        'content-type': 'application/json',
-        'authorization': `Bearer ${config.API_TOKEN}`
+        "content-type": "application/json",
+        authorization: `Bearer ${config.API_TOKEN}`,
       },
     })
-      .then(res => {
-        if (!res.ok)
-          return res.json().then(error => Promise.reject(error))
+      .then((res) => {
+        if (!res.ok) return res.json().then((error) => Promise.reject(error));
       })
       .then(() => {
-        this.resetFields(newBookmark)
-        this.context.updateBookmark(newBookmark)
-        this.props.history.push('/bookmarks')
+        this.resetFields(newBookmark);
+        this.context.updateBookmark(newBookmark);
+        this.props.history.push("/bookmarks");
       })
-      .catch(error => {
-        console.error(error)
-        this.setState({ error })
-      })
-  }
+      .catch((error) => {
+        console.error(error);
+        this.setState({ error });
+      });
+  };
 
   resetFields = (newFields) => {
     this.setState({
-      id: newFields.id || '',
-      title: newFields.title || '',
-      url: newFields.url || '',
-      description: newFields.description || '',
-      rating: newFields.rating || '',
-    })
-  }
+      id: newFields.id || "",
+      title: newFields.title || "",
+      url: newFields.url || "",
+      description: newFields.description || "",
+      rating: newFields.rating || "",
+    });
+  };
 
   handleClickCancel = () => {
-    this.props.history.push('/bookmarks')
+    this.props.history.push("/bookmarks");
   };
 
   render() {
-    const {
-      error,
-      title,
-      url,
-      description,
-      rating
-    } = this.state;
+    const { error, title, url, description, rating } = this.state;
 
     return (
       <section className="EditBookmark">
         <h2>Edit Bookmark</h2>
-        <form
-          className="AddBookmark__form"
-          onSubmit={this.handleSubmit}>
-          <div
-            className="AddBookmark__error"
-            role="alert">
+        <form className="AddBookmark__form" onSubmit={this.handleSubmit}>
+          <div className="AddBookmark__error" role="alert">
             {error && <p>{error.message}</p>}
           </div>
-          <input
-            type="hidden"
-            name="id"
-          />
+          <input type="hidden" name="id" />
           <div>
             <label htmlFor="title">
               Title <Required />
@@ -138,7 +121,7 @@ class EditBookmark extends Component {
                 required
                 value={title}
                 onChange={this.handleChange}
-                />
+              />
             </label>
           </div>
           <div>
@@ -188,7 +171,7 @@ class EditBookmark extends Component {
           </div>
         </form>
       </section>
-    )
+    );
   }
 }
 
